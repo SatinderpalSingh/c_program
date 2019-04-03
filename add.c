94d9d4c7c8651a49f55638ceb2a5369bf1f34c5f
SatinderpalSingh <satinder.goraya91@gmail.com>
2019-04-03 13:45:48 +0530

    basic

---
+++ add.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+main()
+{
+int a,b,sum;
+a = 4;
+b = 6;
+sum = a+b;
+printf("sum is %d", sum);
+}
---
+++ first.c
@@ -0,0 +1,5 @@
+#include<stdio.h>
+main()
+{
+printf("\nHi \t	this is first function\n");
+}
